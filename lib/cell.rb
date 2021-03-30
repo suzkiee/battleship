c02c24faa9978bd6fb4e798bcef8326debd92e33
Suzie K <suziekim.dev@gmail.com>
2021-03-29 20:44:47 -0400

    Complete fire_upon methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,12 @@
 class Cell
   attr_reader :coordinate,
-              :ship
+              :ship,
+              :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -14,5 +16,18 @@ class Cell
 
   def place_ship(player_ship)
     @ship = player_ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @ship != nil 
+      ship.hit 
+      @fired_upon = true
+    else
+      @fired_upon = true
+    end
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -31,6 +31,14 @@ describe Cell do
 
       expect(cell.empty?).to eq true  
     end
+
+    it 'returns false when a ship is present' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+
+      expect(cell.empty?).to eq false
+    end
   end
 
   describe '#place_ship' do
@@ -42,4 +50,33 @@ describe Cell do
       expect(cell.ship).to eq cruiser
     end
   end
-end
+
+  describe '#fired_upon?' do
+    it 'returns false if ship has not been hit' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+
+      expect(cell.fired_upon?).to eq false
+    end
+  end
+
+  describe '#fire_upon' do
+    it 'recognizes it has been fired upon' do
+      cell = Cell.new("B4")
+      cell.fire_upon
+
+      expect(cell.fired_upon?).to eq true
+    end
+
+    it 'hits the ship' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      cell.fire_upon
+
+      expect(cell.fired_upon).to eq true
+      expect(cell.ship.health).to eq 2
+    end
+  end
+end 
