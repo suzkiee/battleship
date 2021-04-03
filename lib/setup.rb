9ab79ef617caf1ce5b9483f15581235e1f62da4d
Suzie K <suziekim.dev@gmail.com>
2021-04-03 17:07:43 -0400

    Create and test place_computer_ship method

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,9 +1,39 @@
 class Setup
-  def initialize(board, ships)
-    @board = board
-    @ships = ships
+  attr_reader :player,
+              :board
+
+  def initialize(board, ships, player)
+    @board  = board
+    @ships  = ships
+    @player = player
+  end
+   # it will be a string concatenation of a sample of A..D and a sample of 1..4
+    # it will need the ships to check ship length to check how many random cells are needed
+    # for each cell, it will randomly generate a letter and a number and then return array 
+    # while valid_placement = false, loop. 
+
+
+  def place_computer_ship(ship, board)
+    placement = random_coordinates(ship, board)
+    while board.valid_placement?(ship, placement) == false
+      placement = random_coordinates(ship, board)
+    end 
+    placement
+  end
+
+  def random_coordinates(ship, board)
+    length = ship.length
+    coordinates = []
+
+    while length > 0
+      length -= 1
+      coordinates << board.cells.keys.sample
+    end
+    return coordinates
   end
 end
+
+
 
 ## need to instantialize this twice in the Game class
 ## need instance variable that tells us which kind of player
--- spec/setup_spec.rb
+++ spec/setup_spec.rb
@@ -5,19 +5,43 @@ require './lib/ship'
 require './lib/setup'
 
 describe Setup do
+
   describe '#initialize' do
+    
     it 'exists' do
       board = Board.new
       submarine = Ship.new("Submarine", 2)
       cruiser = Ship.new("Cruiser", 3)
       ships = [submarine, cruiser]
-      setup = Setup.new(board, ships)
+      setup = Setup.new(board, ships, :computer)
 
       expect(setup).is_a? Setup
     end
+
+    it 'knows who is playing' do
+      board = Board.new
+      submarine = Ship.new("Submarine", 2)
+      cruiser = Ship.new("Cruiser", 3)
+      ships = [submarine, cruiser]
+      setup = Setup.new(board, ships, :computer)
+
+      expect(setup.player).to eq :computer
+    end
   end
 
-  # describe '#setup_board' do
-  #
-  # end
+  describe '#place_computer_ship' do
+    
+    it 'provides valid placement arrays for computer ships' do
+      #computer randomly selects array of cells for each ship until it get valid placement.
+      board = Board.new
+      submarine = Ship.new("Submarine", 2)
+      cruiser = Ship.new("Cruiser", 3)
+      ships = [submarine, cruiser]
+      computer = Setup.new(board, ships, :computer)
+      placement = computer.place_computer_ship(submarine, board)
+      
+      expect(computer.place_computer_ship(submarine, board)).is_a? Array
+      expect(board.valid_placement?(submarine, placement)).to eq true
+    end
+  end
 end
