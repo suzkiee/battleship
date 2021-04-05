4a1f1884061666a920672aa2bebd0a380d6361ef
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-05 15:53:58 -0600

    Create and pseudocode Game class

---
+++ lib/game.rb
@@ -0,0 +1,23 @@
+class Game
+  # main menu method
+    # player can either play or quit
+
+  # complete_setup method
+    # unboxing? where does this live? (instantializing boards, ships, etc.)
+
+  #play game
+
+  # take_turns method
+
+  #check_for_winner method
+
+  # celebrate_winner
+
+  #game.play
+    # => while input == p
+    # complete_setup, take_turns, celebrate_winner
+    # return to main menu when done
+    # check input again, etc. 
+
+
+end
