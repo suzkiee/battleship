eb7c6fa5d59464661e8088cf9107c58dc1b809a4
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-07 16:21:03 -0600

    Make helper methods private

--- lib/board.rb
+++ lib/board.rb
@@ -12,6 +12,77 @@ class Board
     create_hash(keys)
   end
 
+  def valid_placement?(ship, coordinates)
+    all_coordinates_valid?(coordinates) &&
+    correct_placement_length?(ship, coordinates) &&
+    all_cells_available?(coordinates) &&
+    ship_perpendicular?(coordinates)
+  end
+
+  def valid_coordinate?(coordinate)
+    cells.keys.include?(coordinate)
+  end
+
+  def all_coordinates_valid?(coordinates)
+    coordinates.all? do |coordinate|
+      valid_coordinate?(coordinate)
+    end
+  end
+
+  def place(ship, coordinates)
+    if valid_placement?(ship, coordinates)
+      coordinates.each do |coordinate|
+        cells[coordinate].place_ship(ship)
+      end
+    end
+  end
+
+  def render(is_transparent = false)
+    rows = render_cells(is_transparent)
+    row_hash = create_row_hash(rows)
+    return "               1   2   3   4 \n" +
+           "            A #{row_hash["A"]} \n" +
+           "            B #{row_hash["B"]} \n" +
+           "            C #{row_hash["C"]} \n" +
+           "            D #{row_hash["D"]} \n"
+  end
+
+  private 
+
+  def ship_perpendicular?(coordinates)
+    letters = letters(coordinates)
+    numbers = numbers(coordinates)
+    vertical = not_all_same?(letters) && all_same?(numbers) && consecutive?(letters)
+    horizontal = all_same?(letters) && not_all_same?(numbers) && consecutive?(numbers)
+    vertical || horizontal
+  end
+
+  def correct_placement_length?(ship, coordinates)
+    ship.length == coordinates.length
+  end
+
+  def all_cells_available?(coordinates)
+    coordinates.all? do |coordinate|
+      cells[coordinate].empty?
+    end
+  end
+
+  def render_cells(is_transparent = false)
+    rendered_cells = @cells.map do |coordinate, cell|
+      cell.render(is_transparent)
+    end
+
+    rows = rendered_cells.each_slice(4).to_a
+    rows.map do |row|
+      row.join('  ')
+    end
+  end
+
+  def create_row_hash(rows)
+    row_letters = ("A".."D").to_a
+    row_hash = row_letters.zip(rows).to_h
+  end
+
   def create_coordinate_letters
     letters = ["A","B","C","D"].group_by do |letter|
       (1..4).collect {letter}
@@ -41,75 +112,6 @@ class Board
     string_keys = keys.flatten(1).map do |key|
       key.join
     end
-  end
-
-  def valid_placement?(ship, coordinates)
-    all_coordinates_valid?(coordinates) &&
-    correct_placement_length?(ship, coordinates) &&
-    all_cells_available?(coordinates) &&
-    ship_perpendicular?(coordinates)
-  end
-
-  def all_coordinates_valid?(coordinates)
-    coordinates.all? do |coordinate|
-      valid_coordinate?(coordinate)
-    end
-  end
-
-  def ship_perpendicular?(coordinates)
-    letters = letters(coordinates)
-    numbers = numbers(coordinates)
-    vertical = not_all_same?(letters) && all_same?(numbers) && consecutive?(letters)
-    horizontal = all_same?(letters) && not_all_same?(numbers) && consecutive?(numbers)
-    vertical || horizontal
-  end
-
-  def correct_placement_length?(ship, coordinates)
-    ship.length == coordinates.length
-  end
-
-  def all_cells_available?(coordinates)
-    coordinates.all? do |coordinate|
-      cells[coordinate].empty?
-    end
-  end
-
-  def place(ship, coordinates)
-    if valid_placement?(ship, coordinates)
-      coordinates.each do |coordinate|
-        cells[coordinate].place_ship(ship)
-      end
-    end
-  end
-
-  def render(is_transparent = false)
-    rows = render_cells(is_transparent)
-    row_hash = create_row_hash(rows)
-    return "               1   2   3   4 \n" +
-           "            A #{row_hash["A"]} \n" +
-           "            B #{row_hash["B"]} \n" +
-           "            C #{row_hash["C"]} \n" +
-           "            D #{row_hash["D"]} \n"
-  end
-
-  def render_cells(is_transparent = false)
-    rendered_cells = @cells.map do |coordinate, cell|
-      cell.render(is_transparent)
-    end
-
-    rows = rendered_cells.each_slice(4).to_a
-    rows.map do |row|
-      row.join('  ')
-    end
-  end
-
-  def create_row_hash(rows)
-    row_letters = ("A".."D").to_a
-    row_hash = row_letters.zip(rows).to_h
-  end
-
-  def valid_coordinate?(coordinate)
-    cells.keys.include?(coordinate)
   end
 
   def letters(coordinates)
